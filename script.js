faad4e02d2a10d8de9f1600a67a617df061b3189
Hanni <128164992+Hannisiddiqui@users.noreply.github.com>
2023-09-16 20:29:59 +0530

    working

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+function generateAlpaca() {
+    // You can add JavaScript code here to generate random alpaca images.
+    // For simplicity, let's assume it replaces the existing image with a new one.
+    const alpacaImage = document.querySelector('.alpaca-image');
+    alpacaImage.src = 'new_alpaca.png'; // Replace with the actual image source.
+}
+</script>
